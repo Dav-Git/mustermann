96026cc01e15252a0ae9de84727e8531aa4d2e20
David Bauch <david.bauch@capgemini.com>
2021-10-18 11:41:07 +0200

    <vböo<ubväpBA

--- stats_display.php
+++ stats_display.php
@@ -1,3 +1,3 @@
 <?php
-$date_diff = date_diff(new DateTime(strval($_SESSION['time'])), new DateTime());
+$date_diff = date_diff(new DateTime(), new DateTime(strval($_SESSION['time'])));
 echo "Sie sind seit " . $date_diff->i . " Minuten und " . $date_diff->s . " Sekunden hier und haben sich " . $pages . " Seiten angesehen.";
