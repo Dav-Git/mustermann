8ffb930aa0cc5e29f6e5e4f786ad3640bc2acc6e
David Bauch <david.bauch@capgemini.com>
2021-10-25 11:07:24 +0200

    voll deng nicen amgebots giebts hir

--- notebooks.php
+++ notebooks.php
@@ -23,9 +23,7 @@ require("notebook-functions.php")
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
                     <span class="navbar-brand h1">Mustermann GmbH</span>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -40,8 +38,7 @@ require("notebook-functions.php")
                                 <a class="nav-link" href="#">Online-Shop</a>
                             </li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Service
                                 </a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -57,8 +54,7 @@ require("notebook-functions.php")
                         </ul>
                         <form class="d-flex">
                             <input class="form-control me-2" type="search" placeholder="Suche" aria-label="Search">
-                            <button class="btn btn-outline-dark" type="submit"><img
-                                    src="bootstrap5.0.1/icons/search.svg"></button>
+                            <button class="btn btn-outline-dark" type="submit"><img src="bootstrap5.0.1/icons/search.svg"></button>
                         </form>
                     </div>
                 </div>
